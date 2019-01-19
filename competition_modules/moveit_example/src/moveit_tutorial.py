1cee93edbd040d6dc0f48c913d91e602816db678
劉德冠 <43289119+energy7141996@users.noreply.github.com>
2019-01-19 20:21:02 +0800

    Update moveit_tutorial.py

--- competition_modules/moveit_example/src/moveit_tutorial.py
+++ competition_modules/moveit_example/src/moveit_tutorial.py
@@ -57,10 +57,10 @@ class moveit_tutorial(object):
 
                 ### close gripper
 
-                rospy.sleep(2)
-                grip_data = Float64()
-                grip_data.data = 1.2
-                self.pub_gripper.publish(grip_data)
+                #rospy.sleep(2)
+                #grip_data = Float64()
+                #grip_data.data = 1.2
+                #self.pub_gripper.publish(grip_data)
 
 		### Go home
 		self.home() 
